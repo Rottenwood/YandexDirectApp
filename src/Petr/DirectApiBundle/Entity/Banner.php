01f26429dd4af6ea71d439d0a21ed94c88b19c68
Petr Karmashev (Rottenwood) <petr@quank.org>
2014-09-01 17:01:25 +0400

    Создание сущностей для Кампаний и Баннеров

---
+++ src/Petr/DirectApiBundle/Entity/Banner.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace Petr\DirectApiBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Banner
+ * @ORM\Table(name="banners")
+ * @ORM\Entity(repositoryClass="Petr\DirectApiBundle\Repository\BannerRepository")
+ */
+class Banner {
+
+    /**
+     * @var integer
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var integer
+     * @ORM\Column(name="dailyclicks", type="integer")
+     */
+    private $dailyclicks;
+
+    /**
+     * @var integer
+     * @ORM\Column(name="dailycosts", type="integer")
+     */
+    private $dailycosts;
+
+    /**
+     * @var integer
+     * @ORM\Column(name="weeklyclicks", type="integer")
+     */
+    private $weeklyclicks;
+
+    /**
+     * @var integer
+     * @ORM\Column(name="weeklycosts", type="integer")
+     */
+    private $weeklycosts;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Camp")
+     * @ORM\JoinColumn(name="camp_id", referencedColumnName="id")
+     */
+
+    private $campId;
+
+
+    /**
+     * Get id
+     * @return integer
+     */
+    public function getId() {
+        return $this->id;
+    }
+
+    /**
+     * Set dailyclicks
+     * @param integer $dailyclicks
+     * @return Banner
+     */
+    public function setDailyclicks($dailyclicks) {
+        $this->dailyclicks = $dailyclicks;
+
+        return $this;
+    }
+
+    /**
+     * Get dailyclicks
+     * @return integer
+     */
+    public function getDailyclicks() {
+        return $this->dailyclicks;
+    }
+
+    /**
+     * Set dailycosts
+     * @param integer $dailycosts
+     * @return Banner
+     */
+    public function setDailycosts($dailycosts) {
+        $this->dailycosts = $dailycosts;
+
+        return $this;
+    }
+
+    /**
+     * Get dailycosts
+     * @return integer
+     */
+    public function getDailycosts() {
+        return $this->dailycosts;
+    }
+
+    /**
+     * Set weeklyclicks
+     * @param integer $weeklyclicks
+     * @return Banner
+     */
+    public function setWeeklyclicks($weeklyclicks) {
+        $this->weeklyclicks = $weeklyclicks;
+
+        return $this;
+    }
+
+    /**
+     * Get weeklyclicks
+     * @return integer
+     */
+    public function getWeeklyclicks() {
+        return $this->weeklyclicks;
+    }
+
+    /**
+     * Set weeklycosts
+     * @param integer $weeklycosts
+     * @return Banner
+     */
+    public function setWeeklycosts($weeklycosts) {
+        $this->weeklycosts = $weeklycosts;
+
+        return $this;
+    }
+
+    /**
+     * Get weeklycosts
+     * @return integer
+     */
+    public function getWeeklycosts() {
+        return $this->weeklycosts;
+    }
+
+    /**
+     * Set campId
+     * @param integer $campId
+     * @return Banner
+     */
+    public function setCampId($campId) {
+        $this->campId = $campId;
+
+        return $this;
+    }
+
+    /**
+     * Get campId
+     * @return integer
+     */
+    public function getCampId() {
+        return $this->campId;
+    }
+}
---
+++ src/Petr/DirectApiBundle/Entity/Camp.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace Petr\DirectApiBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Camp
+ * @ORM\Table(name="campaigns")
+ * @ORM\Entity(repositoryClass="Petr\DirectApiBundle\Repository\CampRepository")
+ */
+class Camp {
+
+    /**
+     * @var integer
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var integer
+     * @ORM\Column(name="dailyclicks", type="integer")
+     */
+    private $dailyclicks;
+
+    /**
+     * @var integer
+     * @ORM\Column(name="dailycosts", type="integer")
+     */
+    private $dailycosts;
+
+    /**
+     * @var integer
+     * @ORM\Column(name="weeklyclicks", type="integer")
+     */
+    private $weeklyclicks;
+
+    /**
+     * @var integer
+     * @ORM\Column(name="weeklycosts", type="integer")
+     */
+    private $weeklycosts;
+
+
+    /**
+     * Get id
+     * @return integer
+     */
+    public function getId() {
+        return $this->id;
+    }
+
+    /**
+     * Set dailyclicks
+     * @param integer $dailyclicks
+     * @return Camps
+     */
+    public function setDailyclicks($dailyclicks) {
+        $this->dailyclicks = $dailyclicks;
+
+        return $this;
+    }
+
+    /**
+     * Get dailyclicks
+     * @return integer
+     */
+    public function getDailyclicks() {
+        return $this->dailyclicks;
+    }
+
+    /**
+     * Set dailycosts
+     * @param integer $dailycosts
+     * @return Camps
+     */
+    public function setDailycosts($dailycosts) {
+        $this->dailycosts = $dailycosts;
+
+        return $this;
+    }
+
+    /**
+     * Get dailycosts
+     * @return integer
+     */
+    public function getDailycosts() {
+        return $this->dailycosts;
+    }
+
+    /**
+     * Set weeklyclicks
+     * @param integer $weeklyclicks
+     * @return Camps
+     */
+    public function setWeeklyclicks($weeklyclicks) {
+        $this->weeklyclicks = $weeklyclicks;
+
+        return $this;
+    }
+
+    /**
+     * Get weeklyclicks
+     * @return integer
+     */
+    public function getWeeklyclicks() {
+        return $this->weeklyclicks;
+    }
+
+    /**
+     * Set weeklycosts
+     * @param integer $weeklycosts
+     * @return Camps
+     */
+    public function setWeeklycosts($weeklycosts) {
+        $this->weeklycosts = $weeklycosts;
+
+        return $this;
+    }
+
+    /**
+     * Get weeklycosts
+     * @return integer
+     */
+    public function getWeeklycosts() {
+        return $this->weeklycosts;
+    }
+}
