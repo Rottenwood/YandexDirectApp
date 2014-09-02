36d83ce59db9a09dc7b213e569f26a7d1d8ef066
Petr Karmashev (Rottenwood) <petr@quank.org>
2014-09-02 13:05:42 +0400

    Добавлен .htaccess

--- src/Petr/DirectApiBundle/Controller/DefaultController.php
+++ src/Petr/DirectApiBundle/Controller/DefaultController.php
@@ -4,10 +4,9 @@ namespace Petr\DirectApiBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
-{
-    public function indexAction($name)
-    {
-        return $this->render('PetrDirectApiBundle:Default:index.html.twig', array('name' => $name));
+class DefaultController extends Controller {
+
+    public function indexAction() {
+        return $this->render('PetrDirectApiBundle:Default:index.html.twig');
     }
 }
